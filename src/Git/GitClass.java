c506cc7e4aa960a89ce0d1f70883b9cbdd38bd2a
Ranga1115 <suryarangas1995@gmail.com>
2022-07-16 18:38:50 +0530

    Ranga done his coding

--- src/Git/GitClass.java
+++ src/Git/GitClass.java
@@ -3,8 +3,13 @@ package Git;
 public class GitClass {
 	
 	public void example() {
-		System.out.println("hello everyone ");
+		System.out.println("hello everyone "); 
 
+	}
+	
+	private void sample() {
+		
+		System.out.println("hi every one");
 	}
 
 }
